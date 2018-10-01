b3a59c7ed9369bada3d96aa9f34ee5a5681392b7
KIM, Aha <aha00a@gmail.com>
2018-10-01 16:38:51 +0900

    fix bug. style `white-space: nowrap` is added to class `.wiki-fn-content` in site css. so, remove dependency.

--- inline.js
+++ inline.js
@@ -1,4 +1,3 @@
 $('.wiki-fn-content').each(function(i, v) {
-	$('<span></span>').addClass('wiki-fn-content').html($($(v).attr('href')).parent().clone().html().replace(/.+?<\/a>/, '')).insertAfter($(v));
+    $('<sup></sup>').html($($(v).attr('href')).parent().clone().html().replace(/.+?<\/a>/, '')).insertAfter($(v));
 });
-
